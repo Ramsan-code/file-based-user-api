0884c4e37b36fe6cf4ec7fc67c0e0236aa53aef1
thaventhiran Ramsan <ramsandota@gmail.com>
2025-09-21 23:21:09 +0530

    correct code

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+// console.log("ethfs");
+// import express from "express";
+// // const express = require("express");
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Express!");
+// });
+
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
-const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoutes);
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
+});
+
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
